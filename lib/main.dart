d06e17250d7e33b147082e92dd05b0948b342b0f
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-19 22:47:57 -0300

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,6 +20,13 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class Piada {
+  final String pergunta;
+  final String resposta;
+
+  Piada({required this.pergunta, required this.resposta});
+}
+
 class AppiadasScreen extends StatefulWidget {
   const AppiadasScreen({super.key});
 
@@ -26,6 +35,60 @@ class AppiadasScreen extends StatefulWidget {
 }
 
 class _AppiadasScreenState extends State<AppiadasScreen> {
+  final List<Piada> _piadas = [
+    Piada(
+     pergunta: "Porque o desenvolvedor levou o computador ao médico?",
+     resposta: "Pois ele estava com vírus"
+     ),
+    Piada(
+      pergunta: "Por que o astronauta terminou com a namorada?", 
+      resposta: "Porque precisava de espaço."),
+    Piada(
+      pergunta: "Por que o livro de matemática tá sempre triste?" , 
+      resposta: "Porque tem muitos problemas."),
+      Piada(
+      pergunta: "Qual é o animal mais antigo? A zebra..." , 
+      resposta: "Porque é preto e branco desde sempre."),
+      Piada(
+      pergunta: "Por que o jacaré tirou o filho da escola?" , 
+      resposta: "Porque ele réptil de ano."),
+      Piada(
+      pergunta: "O que um raio disse para o outro?" , 
+      resposta: "Nossa, você é tão 'chocante'!"),
+      Piada(
+      pergunta: "O que o peixe faz quando está feliz?" , 
+      resposta: "Nada!"),
+      Piada(
+      pergunta: "Qual é o animal que não vale mais nada?" , 
+      resposta: "O Javali"),
+      Piada(
+      pergunta: "Por que o martelo foi expulso da balada?" , 
+      resposta: "Porque ele estava muito batido"),
+      Piada(
+      pergunta: "Sabe qual a melhor maneira de consumir tempo?" , 
+      resposta: "Comer relógios"),
+      Piada(
+      pergunta: "Sabe qual é a cidade que não aceita táxi?" , 
+      resposta: "Uberlândia!"),
+      Piada(
+      pergunta: "Sabe onde a Barbie comprou as roupas dela?" , 
+      resposta: "Na Barbiea"),
+      Piada(
+      pergunta: "Minha avó tá sem relógio..." , 
+      resposta: "Pois é, já virou uma sem hora")
+  ];
+  Piada _piadaSelecionada = Piada(
+    pergunta: "Aqui vai uma piada", 
+    resposta: "Clique no botão abaixo");
+
+  void _trocarPiada() {
+    final index = Random().nextInt(_piadas.length);
+    
+    setState(() {
+      _piadaSelecionada = _piadas[index];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,18 +98,36 @@ class _AppiadasScreenState extends State<AppiadasScreen> {
         decoration: BoxDecoration(
           image: DecorationImage(image: AssetImage("images/background_joker.png"), fit: BoxFit.cover)
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center, //Vertical
-          crossAxisAlignment: CrossAxisAlignment.center, //Horizontal
-          children: [
-            Text("Aqui vão as piadas", 
-            style: TextStyle(
-              fontSize: 24,
-              fontStyle: FontStyle.italic,
-              fontWeight: FontWeight.bold,
-              color: Colors.orangeAccent
-            )),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center, //Vertical
+            crossAxisAlignment: CrossAxisAlignment.center, //Horizontal
+            children: [
+              Text(
+                "${_piadaSelecionada.pergunta}\n\n${_piadaSelecionada.resposta}", textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 24,
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.bold,
+                  
+                  color: Colors.orangeAccent
+              )),
+
+              SizedBox(height: 24,),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                  ),
+                  onPressed: _trocarPiada,
+                  child: Text("Nova Piada"),
+                )
+              )
+              
+            ],
+          ),
         ),
       ),
     );
